ff7a11f23f20f0c7da2b72b1195e8209f1aa2ce1
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-09-04 21:50:33 +0600

    all crud added

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const router = require('./routes/user.route.js');
-require('dotenv').config();
+const router = require("./routes/user.route.js");
+require("dotenv").config();
 
-// middleware 
-app.use(express.json())
-app.use(cors())
+// middleware
+app.use(express.json());
+app.use(cors());
 // app.use(express.static("public"))
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
+app.use("/user", router);
 
-app.use('/user', router)
-
-
-app.get("/", (req, res) => res.send("Welcome to ACC Assignment1"))
-app.all("*", (req, res) => res.send("No Route Found"))
-app.listen(port, () => console.log("Port is", port))
+app.get("/", (req, res) => res.send("Welcome to ACC Assignment1"));
+app.all("*", (req, res) => res.send("No Route Found"));
+app.listen(port, () => console.log("Port is", port));
